e10d24db87b512bb2035104304babc3c91aab2f9
FoxWithMatches <freiheiit92@mail.ru>
2023-02-08 16:24:51 +0300

    list change

--- src/Containers/List/index.tsx
+++ src/Containers/List/index.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { ListCharacters } from '../../Components/List';
+import { CharactersEffects } from '../../Store';
 
 export const List = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(CharactersEffects.fetchCharacters());
+  }, []);
   return (
     <div>
       <ListCharacters />
